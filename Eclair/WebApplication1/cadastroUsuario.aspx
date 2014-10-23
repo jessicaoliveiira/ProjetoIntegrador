03795b882974fbea1f857a0bb942d2dc184e2088
unknown <Senac2012@CAS419206C14527.senacsp.edu.br>
2014-10-23 20:34:30 -0200

    alteracao nos bagui

--- Eclair/WebApplication1/cadastroUsuario.aspx
+++ Eclair/WebApplication1/cadastroUsuario.aspx
@@ -23,9 +23,9 @@
                  <div><asp:Label ID="Label2" runat="server" Text="Nome de Usuario:"></asp:Label></div>
                  <div><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></div>
                  <div><asp:Label ID="Label3" runat="server" Text="Senha:"></asp:Label></div>
-                 <div><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></div>
+                 <div><asp:TextBox ID="TextBox3" runat="server" TextMode="Password"></asp:TextBox></div>
                  <div><asp:Label ID="Label4" runat="server" Text="Confirmar senha:"></asp:Label></div>
-                 <div><asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></div>
+                 <div><asp:TextBox ID="TextBox4" runat="server" TextMode="Password"></asp:TextBox></div>
                  <div> <asp:Button ID="btnLogin" runat="server" CssClass="bottomCadastrar" Text="Cadastrar" /> </div> 
 
                     </div>
--- Eclair/WebApplication1/login.aspx.designer.cs
+++ Eclair/WebApplication1/login.aspx.designer.cs
@@ -58,13 +58,40 @@ namespace WebApplication1 {
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
         
         /// <summary>
-        /// btnNovo control.
+        /// Label3 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnNovo;
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// TextBox4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
         
         /// <summary>
         /// btnLogin control.
